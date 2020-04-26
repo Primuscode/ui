867a17259747257e1961f203b8f97c8a55f6d5c8
Siddharth <siddharth.kainthola@primussoft.com>
2020-04-26 16:35:52 +0530

    Added color scheme, typography & @angular/material for theming.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 // Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutingModule } from './router';
 
 // Components
@@ -8,7 +9,7 @@ import { CoreComponent } from './components/core.component';
 
 @NgModule({
     declarations: [CoreComponent],
-    imports: [BrowserModule, RoutingModule],
+    imports: [BrowserModule, BrowserAnimationsModule, RoutingModule],
     bootstrap: [CoreComponent]
 })
 
